1431124675fae9ed1602f78b7c7c269e3161f28b
Thorwine <thorwine69@gmail.com>
2022-07-28 12:53:56 +0200

    Add mouseover event to color divs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,23 @@
 const body = document.querySelector('body');
 
-// Container
+// container
 const container = document.createElement('div');
-container.classList.toggle('container')
+container.classList.toggle('myContainer')
 container.style.cssText = 'width: 960px; height: 960px; background-color: darkgrey; display: flex; flex-wrap: wrap;';
 body.appendChild(container);
 
-// Divs
+// grid of divs
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
-  div.classList.toggle('div');
-  div.style.cssText = 'width: 60; height: 60px; background-color: limegreen; flex: 0 0 60px;';
-  div.textContent = 'myDiv';
+  div.classList.toggle('myDiv');
+  div.style.cssText = 'width: 60; height: 60px; background-color: white; display: flex; justify-content: center; align-items: center; flex: 0 0 60px; outline: 1px solid limegreen;';
+  div.textContent = i;
   container.appendChild(div);
+}
+
+// mouseover event
+const div = document.querySelectorAll('.myContainer .myDiv');
+for (let i = 0; i < div.length; i++) {
+  div[i].addEventListener('mouseover', () => div[i].style.backgroundColor = 'red');
 }
 
